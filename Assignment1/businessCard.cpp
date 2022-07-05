3676eb5c07a261bbfa431774c1f585a4f96b0c25
Sam Stansfield <54644448+samstansfield@users.noreply.github.com>
2022-07-05 10:15:33 -0400

    Add files via upload

---
+++ Assignment1/businessCard.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Sam Stansfield" << endl;
+	cout << "Physics Professor/Programmer" << endl;
+	cout << "123-456-7890" << endl;
+	return 0;
+}
